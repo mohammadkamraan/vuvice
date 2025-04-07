3df775dfcf2b131562bed8935114e0b212f015a4
mohammadkamraan <mohammadkammraan@gmail.com>
2025-04-08 00:11:47 +0330

    chore(publish): release version 0.0.3

--- dist/vuevice.js
+++ dist/vuevice.js
@@ -1,91 +1,113 @@
-var f = Object.defineProperty;
-var v = (s, e, r) => e in s ? f(s, e, { enumerable: !0, configurable: !0, writable: !0, value: r }) : s[e] = r;
-var a = (s, e, r) => (v(s, typeof e != "symbol" ? e + "" : e, r), r);
-import { ref as b, computed as d } from "vue";
-class g {
-  constructor() {
-    a(this, "observers", []);
+var b = Object.defineProperty;
+var d = (e, t, s) => t in e ? b(e, t, { enumerable: !0, configurable: !0, writable: !0, value: s }) : e[t] = s;
+var o = (e, t, s) => (d(e, typeof t != "symbol" ? t + "" : t, s), s);
+import { ref as f, computed as v, getCurrentInstance as p, onBeforeMount as S, onBeforeUnmount as g } from "vue";
+const h = /* @__PURE__ */ new Set(), c = /* @__PURE__ */ new Map();
+function y() {
+  const e = f();
+  let t = null;
+  function s(u) {
+    t = u;
   }
-  attach(e) {
-    this.observers.push(e);
-  }
-  detach(e) {
-    this.observers = this.observers.filter(
-      (r) => r !== e
-    );
-  }
-  notify() {
-    for (const e of this.observers)
-      e.update(this);
-  }
-}
-const l = /* @__PURE__ */ new Set(), c = /* @__PURE__ */ new Map();
-function p() {
-  const s = b();
-  let e = null;
-  function r(t) {
-    e = t;
-  }
-  const o = d(() => s.value);
-  function h(t) {
-    if (!l.has(t)) {
-      let u = function(i) {
-        s.value = i;
+  const r = v(() => e.value);
+  function a(u) {
+    if (!h.has(u)) {
+      let n = function(i) {
+        e.value = i;
       };
-      c.set(t, u), e == null || e.attach(u);
+      c.set(u, n), t == null || t.attach(n);
     }
-    l.add(t);
+    h.add(u);
   }
-  function n(t) {
-    const u = c.get(t);
-    u && (e == null || e.detach(u), c.delete(t));
+  function l(u) {
+    const n = c.get(u);
+    n && (t == null || t.detach(n), c.delete(u));
   }
   return {
-    getValue: o,
-    attach: h,
-    detach: n,
-    setObserver: r,
+    getValue: r,
+    attach: a,
+    detach: l,
+    setObserver: s,
     updaters: c
   };
 }
-function V(s, e) {
-  return function(o, h, n) {
-    const t = p();
-    function u(i) {
-      return t.setObserver(this[s]), t.attach(i), {
-        value: t.updaters.has(i) ? e(t.getValue.value) : this[s].getValue(),
-        unSubscribe: t.detach
+function m(e, t) {
+  return function(r, a, l) {
+    const u = y();
+    function n(i) {
+      return u.setObserver(this[e]), u.attach(i), {
+        value: u.updaters.has(i) ? t(u.getValue.value) : this[e].getValue(),
+        unSubscribe: u.detach
       };
     }
-    n.value = u;
+    l.value = n;
   };
 }
-class x {
-  constructor(e) {
-    a(this, "_value");
-    a(this, "observers", []);
-    this.next(e);
+function x() {
+  const e = p();
+  return {
+    uuId: e == null ? void 0 : e.uid.toString()
+  };
+}
+function B(e) {
+  const t = f(e.getValue());
+  function s(a) {
+    t.value = a;
   }
-  attach(e) {
-    this.observers.push(e);
+  const r = v(() => t.value);
+  return S(() => {
+    e.attach(s);
+  }), g(() => {
+    e.detach(s);
+  }), {
+    getValue: r
+  };
+}
+class C {
+  constructor(t) {
+    o(this, "_value");
+    o(this, "observers", []);
+    this.next(t);
   }
-  detach(e) {
-    this.observers = this.observers.filter((r) => r !== e);
+  attach(t) {
+    this.observers.push(t);
+  }
+  detach(t) {
+    this.observers = this.observers.filter((s) => s !== t);
   }
   notify() {
-    for (const e of this.observers)
-      typeof e == "function" ? e(this._value) : e.update(this._value);
+    for (const t of this.observers)
+      typeof t == "function" ? t(this._value) : t.update(this._value);
   }
-  next(e) {
-    typeof e == "function" ? this._value = e(this._value) : this._value = e, this.notify();
+  next(t) {
+    typeof t == "function" ? this._value = t(this._value) : this._value = t, this.notify();
   }
   getValue() {
     return this._value;
   }
 }
+class j {
+  constructor() {
+    o(this, "observers", []);
+  }
+  attach(t) {
+    this.observers.push(t);
+  }
+  detach(t) {
+    this.observers = this.observers.filter(
+      (s) => s !== t
+    );
+  }
+  notify() {
+    for (const t of this.observers)
+      t.update(this);
+  }
+}
 export {
-  g as AbstractSubject,
-  x as BehaviorSubject,
-  V as UiSync,
-  p as useUiSync
+  j as AbstractSubject,
+  C as BehaviorSubject,
+  m as UiSync,
+  x as useComponentId,
+  B as useSubscribe,
+  y as useUiSync
 };
